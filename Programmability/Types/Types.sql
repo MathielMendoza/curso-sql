9f52161da36e502d0af5b142fda63428c183cbf8
Gerson Mendoza <gerbo0906@gmail.com>
2023-04-14 13:08:01 -0600

    Se agregan definicion de tipos de datos

---
+++ Programmability/Types/Types.sql
@@ -0,0 +1,12 @@
+CREATE TYPE [dbo].[historia] FROM [INT] NOT NULL
+    GO
+CREATE TYPE [dbo].[medico] FROM [INT] NOT NULL
+    GO
+CREATE TYPE [dbo].[observacion] FROM [VARCHAR](1000) NULL
+    GO
+CREATE TYPE [dbo].[paciente] FROM [INT] NOT NULL
+    GO
+CREATE TYPE [dbo].[pais] FROM [CHAR](3) NULL
+    GO
+CREATE TYPE [dbo].[turno] FROM [INT] NOT NULL
+    GO
