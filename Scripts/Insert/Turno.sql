f1d4e13e21f7345ffb7f8defe31992c06f367401
Gerson Mendoza <gerbo0906@gmail.com>
2023-04-14 13:28:25 -0600

    Se agregan scripts para agregar datos

---
+++ Scripts/Insert/Turno.sql
@@ -0,0 +1,4 @@
+INSERT INTO turno VALUES (N'20200302 13:00', 0, N'')
+INSERT INTO turno VALUES (N'20200303 14:00', 0, N'')
+INSERT INTO turno VALUES (N'20200303 15:30', 1, N'El paciente ha sido atendido')
+INSERT INTO turno VALUES (N'20200305 18:00', 2, N'El paciente llamó para cancelar el turno')
